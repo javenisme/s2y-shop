14bda90c05f9abab7a6c528d621c2baa34ead33d
Javen <javenisme@gmail.com>
2025-05-25 21:03:43 -0400

    feat: fix logo in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,19 @@
 import { LoginForm } from "@/ui/login-form";
-import { ShirtIcon } from "lucide-react";
+import Image from "next/image";
 
 export default function LoginPage() {
 	return (
 		<div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-neutral-50 p-6 md:p-10">
 			<div className="flex w-full max-w-sm flex-col gap-6">
 				<a href="#" className="flex items-center gap-2 self-center font-medium">
-					<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-						<ShirtIcon className="size-4" />
-					</div>
-					ACME Inc.
+					<Image src="/s2y_logo_dark.png" alt="S2Y Logo" width={60} height={16} className="dark:hidden" />
+					<Image
+						src="/s2y_logo_light.png"
+						alt="S2Y Logo"
+						width={60}
+						height={16}
+						className="hidden dark:block"
+					/>
 				</a>
 				<LoginForm />
 			</div>

c412702aa15a046af5cf70cd3f629ad8520c7e15
Javen <javenisme@gmail.com>
2025-05-31 23:19:59 -0400

    fix: update privacy link

--- src/ui/login-form.tsx
+++ src/ui/login-form.tsx
@@ -38,8 +38,8 @@ export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRe
 				</CardContent>
 			</Card>
 			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
-				By clicking continue, you agree to our <a href="#">Terms of Service</a> and{" "}
-				<a href="#">Privacy Policy</a>.
+				By clicking continue, you agree to our <a href="https://www.s2y.us/terms-service">Terms of Service</a>{" "}
+				and <a href="https://www.s2y.us/privacy-policy">Privacy Policy</a>.
 			</div>
 		</div>
 	);
